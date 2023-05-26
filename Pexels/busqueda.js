1ded8bb2add6c2b2c0927f97b295177546300162
AliSiaa <alicia.roman.169@eaceuta.com>
2023-05-26 10:20:21 +0200

    Pexels

---
+++ Pexels/busqueda.js
@@ -0,0 +1,23 @@
+/* const lupa = document.querySelector(".contenedorLupa");
+
+lupa.addEventListener("click" , function(){
+    
+    const barraBusqueda = document.querySelector(".cuadro");
+    
+    barraBusqueda.classList.toggle("mostrar");
+    console.log("hola")
+    
+})
+ */
+
+
+const cuadroLupa = document.querySelector(".cuadroLupa");
+
+cuadroLupa.addEventListener("click" , function(){
+    
+    const barraBusqueda = document.querySelector(".cuadro");
+    
+    barraBusqueda.classList.toggle("mostrar");
+    console.log("hola")
+    
+})
