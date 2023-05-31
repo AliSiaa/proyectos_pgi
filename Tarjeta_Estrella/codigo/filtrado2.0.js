5cfd620bedf247f3e0a3adf4fd21b088f1ec8dff
AliSiaa <alicia.roman.169@eaceuta.com>
2023-05-31 09:17:27 +0200

    Cambios2

--- Tarjeta_Estrella/codigo/filtrado2.0.js
+++ Tarjeta_Estrella/codigo/filtrado2.0.js
@@ -18,61 +18,6 @@ categorias.forEach(categoria => {
             
         });
 
-        // /* OPCIÓN 1  CONSUME 1*/
-        // switch(clickCategoria){ //clickCategoria puede ser "verde"...
-        //     // Voy a filtrar la variable clickCategoria
-        //     // En los casos siguientes
-        //     // Si hace match con 1 entonces entra
-        //     case "verde": // si es "verde entra aqui dentro"        
-        //         liVerde.style.padding = "10px";
-        //         liVerde.style.border = "1px solid black";
-        //         /* console.log("verde"); */
-        //     break; // SALGO DEL SWITCH
-        //     case "naranja":
-        //         //
-        //         console.log("naranja");
-        //     break;
-        //     case "rojo":
-        //         //
-        //         console.log("rojo");
-        //     break;
-        //     case "todas":
-        //         //
-        //         console.log("todas");
-        //     break;
-        //     default: // CUANDO clickCategoria no es igual a ningun caso
-        //         console.log("NO EXISTE");
-        // }
-        
-        
-        // /* OPCIÓN 2  CONSUME HASTA QUE ENCUENTRA*/
-        // if(clickCategoria == "verde"){
-        //     console.log("verde");
-        // } else if(clickCategoria == "naranja"){
-        //     console.log("naranja");
-        // } else if(clickCategoria == "rojo"){
-        //     console.log("rojo");
-        // } else if(clickCategoria == "todas"){
-        //     console.log("todas");
-        // }
-
-        // /* OPCIÓN 3  CONSUME TODOS*/
-        // if(clickCategoria == "verde"){
-        //     console.log("verde");
-        // }
-
-        // if(clickCategoria == "naranja"){
-        //     console.log("naranja");
-        // }
-
-        // if(clickCategoria == "rojo"){
-        //     console.log("rojo");
-        // }
-
-        // if(clickCategoria == "todas"){
-        //     console.log("todas");
-        // }
-
 
         
         // si la categoria es distinta de valor todas  
