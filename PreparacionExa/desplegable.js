ec9bbc2cdb4c6bcbaecab51afa35195ed0f806f9
AliSiaa <alicia.roman.169@eaceuta.com>
2023-06-13 21:03:18 +0200

    añadidos

---
+++ PreparacionExa/desplegable.js
@@ -0,0 +1,59 @@
+const tarjetas = document.querySelectorAll(".tarjetas");
+const selecionado = document.querySelector(".seleccionado");
+console.log("hola")
+
+tarjetas.forEach(tarjeta => {
+
+    tarjeta.addEventListener("click", function(){
+        // console.log("click", event)
+
+        /* RESET ESTILOS TARJETAS */
+        tarjetas.forEach(tarjeta => { 
+            tarjeta.style.border = "none";
+            /* tarjeta.classList.toggle("modicarAnchura"); */
+
+        })
+
+
+
+       /* tarjeta */
+       tarjeta.style.background = "white";
+       tarjeta.style.border = "1px solid red";
+       
+       
+       
+       /* para el div del objeto selecionado */
+        /*  console.log("la imagen es:", event.target.attributes.src.nodeValue); */ /* img/2.png */
+
+        const img = tarjeta.querySelector("img");
+
+
+        /* Cojo la imagen de div "SELECCIONADO" */
+        const selecionadoImg = selecionado.querySelector("img.zapatos");
+        selecionadoImg.src = img.src;
+
+        /* h1 */
+        const selecionadoH1 = selecionado.querySelector("h1");
+        selecionadoH1.textContent = tarjeta.dataset.tarjeta;
+
+
+        /* p */
+        const selecionadoP = selecionado.querySelector("#precio");
+        selecionadoP.textContent = tarjeta.dataset.p;
+
+        selecionado.style.display = "block";
+    })
+
+    
+})
+
+function toggleMenu(){
+    const desplegable = document.querySelector(".seleccionado");
+
+    if(desplegable.style.display=="none"){
+        desplegable.style.display = "block";
+
+    }
+
+    else{desplegable.style.display = "none"}
+}
---
+++ Prueba - copia/main.js
@@ -0,0 +1,47 @@
+/* COJO TODOS LOS ELEMENTOS QUE
+TENGO QUE ESCUCHAR O MODIFICAR */
+
+const lista = document.getElementById("miLista");
+// const botonesBorrar = document.querySelectorAll(".borrar");
+
+
+/* ¿Qué tengconst botonReiniciar = document.getElementById("reiniciar");
+o que escuchar? */
+function borrarElemento(id){
+    const elemento = document.getElementById(id);
+    elemento.classList.add('oculto');
+}
+
+
+function reiniciarLista(){
+    const items = document.querySelectorAll('#miLista .flex.');
+    console.log(items)
+    items.forEach( item => {
+        item.classList.remove('oculto');
+    })
+}
+function cambiarColor(icono){
+    icono.classList.toggle('color');
+}
+
+
+
+/* for (let i = 0; i < botonBorrar.length; i++) {
+
+    botonBorrar[i].addEventListener('click', function(event){
+        lista.removeChild(event.target.parenElemenet)
+    })
+    
+}
+ */
+
+function toggleMenu(){
+
+    const menu = document.getElementById("bodyPulsable");
+    
+    if(menu.style.display === "none"){
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
