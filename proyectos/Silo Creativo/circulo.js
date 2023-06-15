54714f45d32fb41dd6ad2811483920db6205e1cf
AliSiaa <alicia.roman.169@eaceuta.com>
2023-06-15 10:22:15 +0200

    carpeta proyectos

---
+++ proyectos/Silo Creativo/circulo.js
@@ -0,0 +1,26 @@
+
+
+const lupa = document.querySelector(".busca");
+
+const dialog = document.querySelector(".cuadroBusqueda");
+
+
+lupa.addEventListener("click", function() {
+  
+  dialog.classList.toggle("circulo");
+
+});
+
+// Cuando queremos quitar una clase que se añade con el toggle
+const quitarX = document.querySelector(".close");
+
+quitarX.addEventListener("click", function() {
+  
+  dialog.classList.toggle("circulo");
+
+});
+
+
+
+
+
