9637c683dd54981900a472347f124129e1ff8e09
AliSiaa <alicia.roman.169@eaceuta.com>
2023-06-19 16:26:32 +0200

    Revisar codigo

--- proyectos/Tarjeta_Estrella/codigo/filtrado2.0.js
+++ proyectos/Tarjeta_Estrella/codigo/filtrado2.0.js
@@ -3,39 +3,39 @@ const articulos = document.querySelectorAll(".tarjetas article");
 const categorias = document.querySelectorAll(".galeria ul li");
 
 categorias.forEach(categoria => {
-    categoria.addEventListener('click', function(){
+    categoria.addEventListener('click', function () {
         const colorSelecionadoParaQueSeMuestre = categoria.getAttribute("data-color");
         // Este selector sirve para coger el elemento li que
         // tenga el atributo data-color="verde"
         // const liVerde = document.querySelector('li[data-color="verde"]');
         // liVerde.style.border = "none";
-        
+
         // Resetear las tarjetas para quye se vean todas
         articulos.forEach(articulo => {
-            
+
             articulo.style.display = "inline-flex";
-  
-            
+
+
         });
 
 
-        
+
         // si la categoria es distinta de valor todas  
         // aplicamos los filtros correspondientes 
         // SI ES DIFERENTE o SI NO ES IGUAL
-        if(colorSelecionadoParaQueSeMuestre != "todas"){
+        if (colorSelecionadoParaQueSeMuestre != "todas") {
             articulos.forEach(articulo => {
-            const articuloQueTengoQueDecidirSiSeMuestraONo = articulo.getAttribute("data-color");
-            if(colorSelecionadoParaQueSeMuestre != articuloQueTengoQueDecidirSiSeMuestraONo){
-                articulo.style.display = "none";
-            }
-            
-        });
+                const articuloQueTengoQueDecidirSiSeMuestraONo = articulo.getAttribute("data-color");
+                if (colorSelecionadoParaQueSeMuestre != articuloQueTengoQueDecidirSiSeMuestraONo) {
+                    articulo.style.display = "none";
+                }
+
+            });
 
 
 
         }
-        
+
     })
 })
 
--- proyectos/Tarjeta_Estrella/codigo/menu.js
+++ proyectos/Tarjeta_Estrella/codigo/menu.js
@@ -1,8 +1,8 @@
-function toggleMenu(){
+function toggleMenu() {
 
     const menu = document.getElementById("menu");
-    
-    if(menu.style.display === "none"){
+
+    if (menu.style.display === "none") {
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
